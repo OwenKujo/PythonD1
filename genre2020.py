17c4c617bd624214073bdc28c5bc5c0b33783f20
Owen Kujo <owenrapeepat@gmail.com>
2022-04-11 22:55:56 +0700

    doinn

--- genre2020.py
+++ genre2020.py
@@ -8,27 +8,11 @@ allsing = []
 test = []
 singcount = []
 df = pd.read_excel("D:\Github Python D1\PythonD1\your_top_songs_2020.xlsx")
-for i in (df["Artist Names"]):
+for i in (df["Type"]):
  if i not in allsingname:
   allsingname.append(i)
 for j in allsingname:
- allsing.append(len(df[df["Artist Names"] == j]))
- singcount.append(len(df[df["Artist Names"] == j]))
+ allsing.append(len(df[df["Type"] == j]))
 for k in range (len(allsingname)):
  print(allsingname[k],allsing[k])
-singcount.sort()
-other = singcount[:24]
-k = singcount[24:]
-sumof = [sum(other)]
-test.append(k)
-test.append(sumof)
-f = list(np.concatenate(test).flat)
-labels = ["Camila Cabello","Cardi B","DOBERMAN INFINITY","Tilly Birds","Room39","Pop Pongkul","Atom Chanakan","Skillet","Maroon5","OneRepublic","Bodyslam","EXILE","Ed Sheeran","Shawn Mendes","Others"]
-color = ["#e6e3dc","#d9d9d9","#dedede","#bfbfbf","#b5b5b5","#969696","#878787","#828282","#6b6b6b","#636363","#575757","#9c9c9c","#c9a540","#b5863a","#000000"]
-explode = [0,0,0,0,0,0,0,0,0,0,0,0,0.2,0.2,0]
-print(test)
-print(f)
-print(singcount)
-plt.pie(f,explode=explode,colors= color,shadow=True,labels=labels,startangle=300)
-plt.title("Artist Names")
-plt.show()
+print(allsingname)
