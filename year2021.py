14fa5d1d125fc6b35f301510a848b85af3e8303a
Owen Kujo <owenrapeepat@gmail.com>
2022-04-12 14:58:00 +0700

    Update year2021.py

--- year2021.py
+++ year2021.py
@@ -11,6 +11,7 @@ df = pd.read_excel("D:\Github Python D1\PythonD1\Book1.xlsx")
 for i in (df["Year out"]):
  if i not in yearname:
   yearname.append(i)
+yearname.sort()
 for j in yearname:
  years.append(len(df[df["Year out"] == j]))
 for k in range (len(yearname)):
