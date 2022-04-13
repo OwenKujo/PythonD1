3c54efe22cd3ff2cd8be9109faffafca0957209d
Owen Kujo <100008549+OwenKujo@users.noreply.github.com>
2022-04-13 16:40:41 +0700

    Add files via upload

--- year2020.py
+++ year2020.py
@@ -27,8 +27,8 @@ f = list(np.concatenate(test).flat)
 print(test)
 print(f)
 color = ("#ed281a","#ba5211","#edc61a","#59c72a","#7d2a79","#2ac7c2","#2aa0c7","#1e62ba","#e864d6","#c9c7c9","#45a4f7","#17e81b")
-labels=["2008 - 2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021"]
-
+labels=["2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021"]
+f.pop(0)
 #plt.figure(figsize=(9, 3))
 plt.bar(labels,f)
 plt.ylabel('Number of the song by year')
