831465c9843f04a19b289240af8f778316ca6d87
Owen Kujo <owenrapeepat@gmail.com>
2022-04-13 15:50:22 +0700

    ggg

--- genre comb.py
+++ genre comb.py
@@ -18,12 +18,12 @@ df = pd.read_excel("D:\Github Python D1\PythonD1\your_top_songs_2020.xlsx")
 for i in (df["Type"]):
  if i not in allsingname:
   allsingname.append(i)
-for q in range(3):
-  allsingname.pop(0)
-allsingname.pop(2)
-allsingname.pop(1)
-allsingname.append("Indie Pop")
 
+allsingname.pop(2)
+allsingname.pop(4)
+
+typename.append("J-pop")
+typename.append("Thai-pop")
 for j in allsingname:
  allsing.append(len(df[df["Type"] == j]))
 for k in range (len(allsingname)):
@@ -31,17 +31,19 @@ for k in range (len(allsingname)):
 for x in (df2["Type"]):
  if x not in typename:
   typename.append(x)
+
 for pp in range(5):
- typename.pop(1)
-for tt in range(2):
- typename.pop(2)
+ typename.pop(3)
 for tt in range(2):
  typename.pop(4)
+for tt in range(2):
+ typename.pop(6)
 for p in typename:
  types.append(len(df2[df2["Type"] == p]))
 for q in range (len(typename)):
  print(typename[q],types[q])
  tcount.append(len(df2[df2["Type"] == q]))
+
 
 genrecomb.append(allsing)
 genrecomb.append(types)
@@ -54,8 +56,8 @@ print(genrecomb)
 
 x_axis = np.arange(len(allsingname))
 
-plt.bar(x_axis -0.1,genrecomb[0],width=0.2,label=2020)
-plt.bar(x_axis +0.1,genrecomb[1],width=0.2,label="2021")
+plt.bar(x_axis -0.15,genrecomb[0],width=0.3,label=2020)
+plt.bar(x_axis +0.15,genrecomb[1],width=0.3,label="2021")
 plt.xticks(x_axis, allsingname)
 plt.xlabel("Genre List")
 plt.title("Genre Compare Of My Playlist")
